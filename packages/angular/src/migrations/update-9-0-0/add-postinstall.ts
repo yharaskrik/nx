1e7e54e8c74649dfef12e62e5f9853c765192f26
Victor Savkin <mail@vsavkin.com>
2019-12-03 20:15:45 -0500

    update postinstall

---
+++ packages/angular/src/migrations/update-9-0-0/add-postinstall.ts
@@ -0,0 +1,26 @@
+import { SchematicContext, Tree } from '@angular-devkit/schematics';
+import { updateJsonInTree } from '@nrwl/workspace';
+import { stripIndents } from '@angular-devkit/core/src/utils/literals';
+
+export default function() {
+  return (host: Tree, context: SchematicContext) => {
+    updateJsonInTree('package.json', json => {
+      json.scripts = json.scripts || {};
+      if (json.scripts.postinstall) {
+        context.logger.warn(
+          stripIndents`
+            ---------------------------------------------------------------------------------------
+            Angular Ivy requires you to run ngcc after every npm install.
+            The easiest way to accomplish this is to update your postinstall script to invoke ngcc.
+            ---------------------------------------------------------------------------------------
+          `
+        );
+      } else {
+        context.logger.info(
+          stripIndents`A "postinstall" script has been added to package.json to run ngcc.`
+        );
+      }
+      return json;
+    });
+  }
+}
